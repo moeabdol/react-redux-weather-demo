fbf21260dfb3afca99e97a800ddb26cd2d4a1615
moeabdol <mohd.a.saed@gmail.com>
2018-01-07 20:31:15 +0300

    Implement maps and charts

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'd0fc78d57e3d1d08f2a3241f8bc47d3c';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export const fetchWeather = city => {
+  const url = `${ROOT_URL}&q=${city}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
